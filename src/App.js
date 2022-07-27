196cc856d88980c95842b2096703cf10ec96d69d
aliceauko <alice.auko@student.moringaschool.com>
2022-07-27 23:45:20 +0300

    Save the data on local storage

--- src/App.js
+++ src/App.js
@@ -1,57 +1,71 @@
-import React,{useState}from "react";
+import React, {useState, useEffect} from "react";
 import Notes from "./components/Notes";
-import {nanoid} from 'nanoid';
+import { nanoid } from "nanoid";
 
-function App(){
-  const[newnote, setNewnote] =useState([
+function App() {
+  const [newnote, setNewnote] = useState([
     {
-    id:nanoid(),
-    write: "This is my first note",
-    date:"15/04/2021",
-  },
-  {
-    id:nanoid(),
-    write: "This is my second note",
-    date:"23/04/2021",
-  },
-  {
-    id:nanoid(),
-    write: "This is my third note",
-    date:"28/04/2021",
-  },
-  {
-    id:nanoid(),
-    write: "This is my fourth note",
-    date:"2/05/2021",
-  }
-  
-  
-]);
+      id: nanoid(),
+      write: "This is my first note",
+      date: "15/04/2021",
+    },
+    {
+      id: nanoid(),
+      write: "This is my second note",
+      date: "23/04/2021",
+    },
+    {
+      id: nanoid(),
+      write: "This is my third note",
+      date: "28/04/2021",
+    },
+    {
+      id: nanoid(),
+      write: "This is my fourth note",
+      date: "2/05/2021",
+    },
+  ]);
 
- const addnewNote = (write)=>{
- const date = new Date();
- const newNote ={
-   id: nanoid(),
-   write: write,
-   date : date.toLocaleDateString()
- }
- const newNotes = [...newnote,newNote];
- setNewnote(newNotes)
- }
+  useEffect(() => {
+    const newnote = JSON.parse(
+      localStorage.getItem("newnote")
+      );
+    if (newnote) {
+      setNewnote(newnote);
+    }
+  }, []);
 
-const deleteNotes =(id)=>{
- const newUpdatedNotes= newnote.filter((note)=> note.id !==id);
- setNewnote(newUpdatedNotes)
+  useEffect(() => {
+    localStorage.setItem(
+      "newnote",
+       JSON.stringify(newnote));
+  }, [newnote]);
 
-}
+  const addnewNote = (write) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      write: write,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...newnote, newNote];
+    setNewnote(newNotes);
+  };
 
-  return(
+  const deleteNotes = (id) => {
+    const newUpdatedNotes = newnote.filter((note) => note.id !== id);
+    setNewnote(newUpdatedNotes);
+  };
+
+  return (
     <div className="contain">
-      <Notes newnote={newnote} handleAddnewNote={addnewNote}
-      handleDeletenote={deleteNotes}/>
-      
+      <Notes
+        newnote={newnote}
+        handleAddnewNote={addnewNote}
+        handleDeletenote={deleteNotes}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
